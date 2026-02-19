5ece30d622d3c7a0aa8f50ad3fbc59f08a7cc204
Kamal Kumar <kamalkumar@Kamals-MacBook-Air.local>
2026-02-19 23:37:49 +0530

    bookmark icon changed to heart icon

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -26,7 +26,7 @@ const Movie = (props) => {
       setPopup(`Movie - "${movie.title}" removed from Watchlist`);
     } else {
       context.addToWatchList(movie);
-      setPopup(`Movie - "${movie.title}" added to Watchlist`)
+      setPopup(`Movie - "${movie.title}" added to Watchlist`);
     }
   };
 
@@ -39,10 +39,10 @@ const Movie = (props) => {
         alt=""
       />
       <div className="flex justify-between items-start mt-2">
-      <figcaption className="flex flex-col ms-2 mb-1">
-        <h3 className="font-bold">{props.title}</h3>
-        <h4>{props.releaseDate}</h4>
-        <h4>{genre && genre.name}</h4>
+        <figcaption className="flex flex-col ms-2 mb-1">
+          <h3 className="font-bold">{props.title}</h3>
+          <h4>{props.releaseDate}</h4>
+          <h4>{genre && genre.name}</h4>
         </figcaption>
         <button
           onClick={addToWatchListHandler}
@@ -52,14 +52,10 @@ const Movie = (props) => {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              fill="currentColor"
+              fill="red"
               className="size-8"
             >
-              <path
-                fillRule="evenodd"
-                d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z"
-                clipRule="evenodd"
-              />
+              <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
             </svg>
           ) : (
             <svg
@@ -73,19 +69,13 @@ const Movie = (props) => {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
               />
             </svg>
           )}
         </button>
-        </div>
-      {popup && (
-  <Popup
-    message={popup}
-    onClose={() => setPopup(null)}
-  />
-)}
-
+      </div>
+      {popup && <Popup message={popup} onClose={() => setPopup(null)} />}
     </figure>
   );
 };
